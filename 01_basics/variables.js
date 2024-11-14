27a0a90dd08330549d7ba19ff9f1483a1847e0da
ImDhairya <pandyadhairya789@gmail.com>
2024-11-14 11:14:51 +0530

    removed js added ts

--- 01_basics/variables.js
+++
@@ -1,9 +0,0 @@
-const accountId = 144553;
-let accountEmail = "dhairyapandya@gmail.com ";
-var accountPassword = "12345";
-
-let accountState;
-console.log("first");
-
-console.table(accountId, accountEmail);
-f
